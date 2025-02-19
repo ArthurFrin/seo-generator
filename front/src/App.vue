93b563705a46d6910b3b6d7782a8f0d879733a2b
Arthur Frin <frin.arthur@gmail.com>
2025-02-19 10:48:36 +0100

    back btn

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import ReturnButton from './components/ReturnButton.vue'
 </script>
 
 <template>
+  <ReturnButton />
   <RouterView />
 </template>
---
+++ front/src/components/ReturnButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goHome = () => {
+    router.push('/');
+};
+</script>
+
+<template>
+    <button @click="goHome" class="return-button bg-purple-600 hover:bg-purple-700">Retour</button>
+</template>
+
+<style scoped>
+.return-button {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    padding: 10px 20px;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    z-index: 1000;
+}
+</style>
